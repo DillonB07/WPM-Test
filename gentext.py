c6ad1aa905f4378b83539397847dd313dda79db5
Sourcery AI <>
2022-02-11 18:28:31 +0000

    'Refactored by Sourcery'

--- gentext.py
+++ gentext.py
@@ -4,6 +4,6 @@ gen = DocumentGenerator()
 
 
 with open('text.txt', 'w') as f:
-    for i in range(1000000):
+    for _ in range(1000000):
         sentence = gen.sentence()
         f.write(f'{sentence}\n')
